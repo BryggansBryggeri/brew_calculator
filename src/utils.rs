9bbeaeaf6a9ebe30dcf1d21ece36721376db4705
Jakob <jakob.lindqvist@chalmers.se>
2019-09-13 17:12:14 +0200

    Unit test f32 almost equal check

--- src/utils.rs
+++ src/utils.rs
@@ -17,3 +17,27 @@ pub fn f32_almost_equal(a: f32, b: f32, tolerance: Option<f32>) -> bool {
         (diff / f32::min(abs_a + abs_b, f32::MAX)) < tolerance
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    /// Test some randomly picked values from here:
+    /// https://www.brewersfriend.com/plato-to-sg-conversion-chart/
+    #[test]
+    fn handles_nan() {
+        assert!(!f32_almost_equal(2.32, f32::NAN, None));
+        assert!(!f32_almost_equal(f32::NAN, 785.4, None));
+    }
+
+    #[test]
+    fn small_numbers() {
+        assert!(f32_almost_equal(0.0001, 0.0001, None));
+        assert!(!f32_almost_equal(f32::MIN_POSITIVE, 0.0, None));
+        assert!(f32_almost_equal(f32::MIN_POSITIVE, f32::MIN_POSITIVE, None));
+    }
+
+    #[test]
+    fn big_numbers() {
+        assert!(f32_almost_equal(f32::MAX, f32::MAX, None));
+    }
+}
