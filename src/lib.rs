1df72035b50e53e28a3fb686f56eae293f979c20
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2019-09-25 19:00:02 +0200

    Notation section in docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 //! # BRYGGANS BRYGGERI BÄRS BB's brew calculator
+//!
+//! ## Notation
+//! Variables are introduced with a symbol and the dimension in squared brackets.
+//!
+//! Ex: Hop mass $m_H \[\text{kg}\]$.
+//!
+//! Dimensionless quantities are denoted $\[-\]$
 
 pub mod calculations;
