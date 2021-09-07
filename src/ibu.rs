80b2bf33bc0df9433adbbcedaa99d5c064e845fc
Jakob <jakob.lindqvist@chalmers.se>
2021-09-07 18:26:53 +0200

    IBU variants as strings
    Not sure if this is the way to go visavi wasm support

--- src/ibu.rs
+++ src/ibu.rs
@@ -5,39 +5,59 @@
 //! - $V$ \[l\]: Average boil volume (estimated with boil-off),
 //! - $t$ \[min\]: Boil time
 //! - $\rho$ \[-\]: Wort gravity.
+
+use crate::units::{Ibu, Kilograms, Liters, Minutes, Percent};
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    concentration::SpecificGravity,
-    units::{Kilograms, Liters, Minutes, Percent},
-};
-
-/// IBU methods
-#[derive(Debug, Deserialize, Serialize, PartialEq, Clone, Copy)]
-pub enum Method {
-    ///See [`tinseth_ibu`]
-    Tinseth,
-    Rager,
-    Garetz,
-    Noonan,
+/// IBU for a single hop addition
+pub trait IbuCalc {
+    /// IBU for a single hop addition
+    fn ibu(
+        self,
+        hop_mass: Kilograms,
+        alpha_acid: Percent,
+        volume: Liters,
+        boil_time: Minutes,
+        wort_gravity: f32,
+    ) -> Ibu;
 }
 
 /// IBU for a single hop addition
 ///
 /// Wrapper for different methods
-pub fn ibu(
-    method: Method,
-    hop_mass: Kilograms,
-    alpha_acid: Percent,
-    volume: Liters,
-    boil_time: Minutes,
-    wort_gravity: f32,
-) -> f32 {
-    match method {
-        Method::Tinseth => tinseth_ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity),
-        Method::Rager => rager_ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity),
-        Method::Garetz => garetz_ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity),
-        Method::Noonan => noonan_ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity),
+/// The variants have an inner struct to allow for more complex computations while keeping a joint
+/// `ibu` interface. Possibly not good for wasm use.
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone, Copy)]
+pub enum Method {
+    /// See [`Tinseth`]
+    Tinseth(Tinseth),
+    Rager,
+    Garetz,
+    Noonan,
+}
+
+impl IbuCalc for Method {
+    fn ibu(
+        self,
+        hop_mass: Kilograms,
+        alpha_acid: Percent,
+        volume: Liters,
+        boil_time: Minutes,
+        wort_gravity: f32,
+    ) -> Ibu {
+        match self {
+            Method::Tinseth(tinseth) => {
+                tinseth.ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity)
+            }
+            Method::Rager => rager_ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity),
+            Method::Garetz => garetz_ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity),
+            Method::Noonan => noonan_ibu(hop_mass, alpha_acid, volume, boil_time, wort_gravity),
+        }
+    }
+}
+impl Default for Method {
+    fn default() -> Self {
+        Self::Tinseth(Tinseth {})
     }
 }
 
@@ -51,18 +71,24 @@ pub fn ibu(
 ///
 /// $U(t, \rho)$ and $C_G(\rho)$ are calculated with [`utilisation`] and
 /// [`gravity_correction_factor`] respectively.
-pub fn tinseth_ibu(
-    hop_mass: Kilograms,
-    alpha_acid: Percent,
-    volume: Liters,
-    boil_time: Minutes,
-    wort_gravity: f32,
-) -> f32 {
-    // The original formula has a factor 1000, however here `hop_mass` is measured in kg
-    // and the `alpha_acid` in percentage, not a fraction.
-    let numerator = 10_000.0 * hop_mass * utilisation(boil_time, wort_gravity) * alpha_acid;
-    let denominator = volume * gravity_correction_factor(wort_gravity);
-    numerator / denominator
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone, Copy)]
+pub struct Tinseth {}
+
+impl IbuCalc for Tinseth {
+    fn ibu(
+        self,
+        hop_mass: Kilograms,
+        alpha_acid: Percent,
+        volume: Liters,
+        boil_time: Minutes,
+        wort_gravity: f32,
+    ) -> Ibu {
+        // The original formula has a factor 1000, however here `hop_mass` is measured in kg
+        // and the `alpha_acid` in percentage, not a fraction.
+        let numerator = 10_000.0 * hop_mass * utilisation(boil_time, wort_gravity) * alpha_acid;
+        let denominator = volume * gravity_correction_factor(wort_gravity);
+        numerator / denominator
+    }
 }
 
 /// Rager IBU for a single hop addition
@@ -169,7 +195,7 @@ mod tests {
     // https://www.backtoschoolbrewing.com/blog/2016/9/5/how-to-calculate-ibus
     #[test]
     fn test_tinseth_ibu() {
-        let calc_ibu = tinseth_ibu(0.007, 8.5, 22.73, 15.0, 1.058);
+        let calc_ibu = Tinseth {}.ibu(0.007, 8.5, 22.73, 15.0, 1.058);
         assert_approx_eq!(calc_ibu, 2.74, 0.05);
     }
 }
